ca2eaa2a413adbfe2c8bf63b182bfdbbfa94ac75
Ryan Strauss <ryanrstrauss@icloud.com>
2018-05-12 23:57:31 -0400

    Integrate Node into AVLTree

--- core/src/trees/AVLTree.java
+++ core/src/trees/AVLTree.java
@@ -433,4 +433,36 @@ public class AVLTree {
         printInOrder(currentRoot.left, indentLevel + 1);
 
     }
+
+    /**
+     * Self balancing binary tree node.
+     *
+     * @author Ryan Strauss
+     */
+    private class Node {
+        int value, level;
+        Node left, right, parent;
+
+        /**
+         * Parameterized constructor for the <code>Node</code> class which sets the value of the node
+         * equal to <code>value</code>.
+         *
+         * @param value the value of the new node
+         */
+        Node(int value) {
+            this.value = value;
+            left = null;
+            right = null;
+            parent = null;
+            level = 0;
+        }
+
+        /**
+         * Returns the <code>Node</code> as a <code>String</code> as "[value, level]".
+         */
+        public String toString() {
+            return "[" + value + ", " + level + "]";
+        }
+    }
+
 }
