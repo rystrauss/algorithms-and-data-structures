fe5ee3fc1af0fc4a7953fa2bfb5980cd50ae9409
Ryan Strauss <ryanrstrauss@icloud.com>
2018-05-15 17:03:55 -0400

    Add TreeSort

---
+++ core/src/sort/TreeSort.java
@@ -0,0 +1,32 @@
+package sort;
+
+import trees.AVLTree;
+
+import java.util.List;
+
+/**
+ * Class containing implementation of binary tree sort.
+ * <p>
+ * The underlying AVL Tree excludes duplicate entries, so this class' sort method
+ * removes duplicates during the sorting process.</p>
+ *
+ * @author Ryan Strauss
+ */
+public class TreeSort {
+
+    /**
+     * Performs a binary tree sort on the given array.
+     *
+     * @param nums the numbers to be sorted
+     * @return an ArrayList containing the elements of nums in sorted order, with duplicate
+     * entries removed
+     */
+    public static List<Integer> sort(int[] nums) {
+        AVLTree tree = new AVLTree();
+        for (int i : nums) {
+            tree.add(i);
+        }
+        return tree.inOrder();
+    }
+
+}
