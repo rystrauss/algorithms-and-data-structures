8e46335a520b95001bbba738883b490231d0ed0e
Ryan Strauss <rystrauss@davidson.edu>
2019-09-25 23:32:01 -0400

    Add tree

---
+++ core/src/tree/Node.java
@@ -0,0 +1,23 @@
+package tree;
+
+/**
+ * Class that represents a node within a tree.
+ *
+ * @param <E> the type of the data contained in the node
+ * @author Ryan Strauss
+ */
+class Node<E extends Comparable<E>> {
+
+    Node<E> parent, left, right;
+    E data;
+    int level;
+
+    Node(E data) {
+        this.data = data;
+        this.parent = null;
+        this.left = null;
+        this.right = null;
+        this.level = 0;
+    }
+
+}
