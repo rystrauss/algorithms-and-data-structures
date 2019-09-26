a9e1ad943e98aa2bad4b410e48aeba85547970ce
Ryan Strauss <rystrauss@davidson.edu>
2019-09-26 16:12:47 -0400

    Add some functionality to lists

--- core/src/list/LinkedList.java
+++ core/src/list/LinkedList.java
@@ -1,5 +1,7 @@
 package list;
 
+import java.util.Iterator;
+
 /**
  * Implementation of a simple singly linked list.
  *
@@ -26,39 +28,41 @@ public class LinkedList<T> implements List<T> {
      * Constructs an empty list.
      */
     public LinkedList() {
-        head = null;
-        tail = null;
-        size = 0;
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
     }
 
     @Override
     public void add(T object) {
         Node newNode = new Node(object);
-        if (head == null) {
-            head = newNode;
-            tail = head;
+        if (this.head == null) {
+            this.head = newNode;
+            this.tail = this.head;
         } else {
-            tail.next = newNode;
-            tail = newNode;
+            this.tail.next = newNode;
+            this.tail = newNode;
         }
-        size++;
+        this.size++;
     }
 
     @Override
     public void add(T object, int index) {
-        if (index < 0 | index > size) {
+        if (index < 0 | index > this.size)
             throw new IndexOutOfBoundsException();
-        }
-        if (index == size) {
+
+        if (index == this.size) {
             add(object);
             return;
         }
+
         Node newNode = new Node(object);
+
         if (index == 0) {
-            newNode.next = head;
-            head = newNode;
+            newNode.next = this.head;
+            this.head = newNode;
         } else {
-            Node curr = head;
+            Node curr = this.head;
             int location = 0;
             while (curr.next != null && location++ < index - 1) {
                 curr = curr.next;
@@ -67,14 +71,14 @@ public class LinkedList<T> implements List<T> {
             curr.next = newNode;
             newNode.next = temp;
         }
-        size++;
+        this.size++;
     }
 
     @Override
     public T get(int index) throws IndexOutOfBoundsException {
-        if (index < 0 || index >= size)
+        if (index < 0 || index >= this.size)
             throw new IndexOutOfBoundsException();
-        Node pos = head;
+        Node pos = this.head;
         for (int i = 0; i < index; i++) {
             pos = pos.next;
         }
@@ -82,10 +86,25 @@ public class LinkedList<T> implements List<T> {
     }
 
     @Override
-    public T remove(int index) throws IndexOutOfBoundsException {
-        if (index < 0 || index >= size)
+    public T set(T element, int index) throws IndexOutOfBoundsException {
+        if (index < 0 || index >= this.size)
             throw new IndexOutOfBoundsException();
-        Node pos = head;
+
+        Node cur = this.head;
+        while (--index >= 0)
+            cur = cur.next;
+
+        T oldValue = cur.value;
+        cur.value = element;
+
+        return oldValue;
+    }
+
+    @Override
+    public T remove(int index) throws IndexOutOfBoundsException {
+        if (index < 0 || index >= this.size)
+            throw new IndexOutOfBoundsException();
+        Node pos = this.head;
         Node temp;
         for (int i = 0; i < index - 1; i++) {
             pos = pos.next;
@@ -96,42 +115,99 @@ public class LinkedList<T> implements List<T> {
     }
 
     @Override
+    public int indexOf(T element) {
+        int index = 0;
+        Node cur = this.head;
+        while (cur != null) {
+            if (cur.value.equals(element))
+                return index;
+            cur = cur.next;
+            index++;
+        }
+        return -1;
+    }
+
+    @Override
+    public boolean remove(T element) {
+        Node cur = this.head;
+
+        while (cur.next != null) {
+            if (cur.next.value.equals(element)) {
+                cur.next = cur.next.next;
+                return true;
+            }
+            cur = cur.next;
+        }
+
+        return false;
+    }
+
+    /**
+     * Reverses the elements of the list.
+     * <p>
+     * This implementation runs in O(n) time.
+     */
     public void reverse() {
         Node prev = null;
         Node next = null;
-        Node cur = head;
+        Node cur = this.head;
         while (cur != null) {
             next = cur.next;
             cur.next = prev;
             prev = cur;
             cur = next;
         }
-        tail = head;
-        head = prev;
+        this.tail = this.head;
+        this.head = prev;
     }
 
     @Override
     public int size() {
-        return size;
+        return this.size;
     }
 
     @Override
     public boolean isEmpty() {
-        return size == 0;
+        return this.size == 0;
     }
 
     @Override
     public String toString() {
-        if (head == null)
+        if (this.head == null)
             return "[]";
         StringBuilder s = new StringBuilder("[");
-        Node curr = head;
+        Node curr = this.head;
         while (curr.next != null) {
             s.append(curr.value).append(", ");
             curr = curr.next;
         }
         s.append(curr.value).append("]");
         return s.toString();
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new LinkedListIterator();
+    }
+
+    private class LinkedListIterator implements Iterator<T> {
+
+        private Node nextNode;
+
+        LinkedListIterator() {
+            this.nextNode = head;
+        }
+
+        public boolean hasNext() {
+            return this.nextNode != null;
+        }
+
+        public T next() {
+            T data = this.nextNode.value;
+            this.nextNode = this.nextNode.next;
+            return data;
+        }
+
     }
 
 }
--- core/src/list/List.java
+++ core/src/list/List.java
@@ -6,7 +6,7 @@ package list;
  * @param <T> The type of the elements in the list.
  * @author Ryan Strauss
  */
-public interface List<T> {
+public interface List<T> extends Iterable<T> {
 
     /**
      * Adds the given integer to the end of the list.
@@ -33,6 +33,16 @@ public interface List<T> {
     public T get(int index) throws IndexOutOfBoundsException;
 
     /**
+     * Replaces the element at the specified position in this list with the specified element.
+     *
+     * @param element element to be stored at the specified position
+     * @param index   index of the element to replace
+     * @return the element previously at the specified position
+     * @throws IndexOutOfBoundsException if the index is out of range
+     */
+    public T set(T element, int index) throws IndexOutOfBoundsException;
+
+    /**
      * Removes the element at the specified index, and returns that element.
      *
      * @param index the index of the element to remove
@@ -42,9 +52,22 @@ public interface List<T> {
     public T remove(int index) throws IndexOutOfBoundsException;
 
     /**
-     * Reverses the list.
+     * Returns the index of the first occurrence of the specified element in this list,
+     * or -1 if this list does not contain the element.
+     *
+     * @param element element to search for
+     * @return the index of the first occurrence of the specified element in this list, or -1 if
+     * this list does not contain the element
      */
-    public void reverse();
+    public int indexOf(T element);
+
+    /**
+     * Removes the first occurrence of the specified element from this list, if it is present.
+     *
+     * @param element the element to remove
+     * @return true iff element was found in the list
+     */
+    public boolean remove(T element);
 
     /**
      * Gets the size of the list.
